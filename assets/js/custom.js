d35b44d31f36272740da627c9404979157985a67
Poshan Soneshwary <poshankumar0610@gmail.com>
2023-08-21 18:55:19 +0530

    Fluid Homepage with Current Progress

---
+++ assets/js/custom.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+  $('.product-slider-wrap').slick({
+    dots: false,
+    infinite: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 4.25,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3.25,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2.25,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+  });
+})
